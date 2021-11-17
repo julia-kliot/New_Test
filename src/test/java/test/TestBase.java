f7225adb0036e96bc5023427439869dd0ba59e1a
julia-kliot <juliakliot.jk@gmail.com>
2021-11-18 02:10:14 +0300

    testng

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -1,6 +1,7 @@
 package test;
 
 import manager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
@@ -11,7 +12,7 @@ import org.testng.annotations.BeforeSuite;
 import java.lang.reflect.Method;
 
 public class TestBase {
-    protected static ApplicationManager app = new ApplicationManager();
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
     Logger logger= LoggerFactory.getLogger(TestBase.class);
     @BeforeSuite
     public void setUp() throws InterruptedException {
